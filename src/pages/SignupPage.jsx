5d2d56bbd459456b5a6322329f840d0261b3e506
AtharvaTandale07 <atharva07tandale@gmail.com>
2024-12-26 19:01:37 +0530

    Changed signin page
    Signed-off-by: AtharvaTandale07 <atharva07tandale@gmail.com>

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import SignupContent from '../components/SignupContent';
-// import HeroComponent from '../components/HeroComponent';
-// import svgIconUrl from '../assets/CodeAntAIIcon.svg';
-
-// const SignupPage = () => {
-//   return (
-//     <div className='w-full min-h-screen'>
-//       <div className='flex min-h-screen'>
-//         <div className='hidden lg:block lg:w-1/2 border-r border-gray-200 bg-white relative'>
-//           <HeroComponent />
-//           <img 
-//             src={svgIconUrl} 
-//             className='absolute bottom-0 w-[284px] h-[319px] opacity-25'
-//             alt="CodeAnt AI Icon"
-//           />
-//         </div>
-//         <div className='w-full lg:w-1/2 bg-[#FAFAFA] flex justify-center items-center'>
-//           <div className='bg-white rounded-lg w-11/12 h-[67%] drop-shadow-lg'>
-//             <SignupContent />
-//             <div className='text-center'>By signing up you agree to the <span className='font-semibold'>Privacy Policy</span></div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignupPage;
-
 import React from 'react';
 import SignupContent from '../components/SignupContent';
 import HeroComponent from '../components/HeroComponent';
